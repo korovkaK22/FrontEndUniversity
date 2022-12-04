140a2ebc65b563cb0bdb20dda6694184be3d3a2a
korovkaK22 <115235686+korovkaK22@users.noreply.github.com>
2022-12-04 20:13:48 +0200

    + ссилки

--- src/components/Validation/InputValidation.js
+++ src/components/Validation/InputValidation.js
@@ -1,10 +1,13 @@
-import axios from 'axios'
+//import axios from 'axios'
 
 export class InputValidation {
 
     static checkName(name){
         let result=true;
-
+        if (name.length<=3 || name.length>=253){
+            result= false;
+        }
+        return result;
     }
 
 
